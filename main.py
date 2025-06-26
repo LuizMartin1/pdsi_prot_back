6297570465f294d93ac6ba2981f2713f2e1a43b8
LuizMartin1 <luizmartinids@gmail.com>
2025-06-26 10:29:37 -0300

    segunda ação

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return{"Hellooooo": "lala"}
+    return{"Hello Lui": "lala"}
 
 @app.post("/criar", status_code=status.HTTP_201_CREATED)
 def criar_valores(nova_mensagem: classes.Mensagem, db: Session = Depends(get_db)):
