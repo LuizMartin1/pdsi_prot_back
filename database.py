11deeb9f83d0f40d86eb8f9069d38e9b8d73a349
LuizMartin1 <luizmartinids@gmail.com>
2025-07-01 11:15:18 -0300

    teste secrets 9

--- database.py
+++ database.py
@@ -2,6 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 import os
+
+# Carrega variáveis do .env apenas localmente (não afeta GitHub Actions)
 load_dotenv()
 
 user = os.getenv("DB_USER")
@@ -13,7 +15,7 @@ SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@{host}/{database}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-SessionLocal = sessionmaker( autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 def get_db():
